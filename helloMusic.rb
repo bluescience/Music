203d34d22bc0b3f30e96c7b6ebc86571bb3edb9d
Ronald Longcape <Ronald Longcape>
2017-04-07 11:39:54 -0700

    git is a pain sometimes

--- helloMusic.rb
+++ helloMusic.rb
@@ -1,3 +1,7 @@
+5.times do
+  sample :elec_blip2
+  sleep 0.5
+end
 3.times do
   play :D
   play :A
@@ -10,3 +14,27 @@
     sleep 0.1
   end
 end
+sleep 0.15
+play :A1
+sleep 0.5
+play :A2
+sleep 0.5
+play :A3
+sleep 0.5
+play :A4
+sleep 0.5
+play :A5
+sleep 0.5
+play :A
+sleep 0.5
+6.times do
+  play :E
+  sleep 0.1
+end
+sleep 0.7
+play :B5
+play :C5
+sleep 0.2
+play :B
+play :C
+
